df6b31a5a76544594f7b352168b65d953f0fb918
Miguel Angel Torres <miguelangeltm@live.com>
2022-02-08 11:25:35 -0500

    Delete src directory

--- src/components/Atoms/text/CardHeading/CardHeading.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import "./CardHeading.css";
-
-const CardHeading = ({name, issueNumber}) => {
-  return <h4>{name + " #" + issueNumber}</h4>;
-};
-
-export default CardHeading;
--- src/components/Atoms/text/SectionTitle/SectionTitle.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const SectionTitle = (props) => {
-
- return <h2>{props.children}</h2>
-};
-
-
-export default SectionTitle;
