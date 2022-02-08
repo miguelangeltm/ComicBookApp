1ae38ca64e6c21c5c6408f508d88836ffa760ae0
Mike Vitamina <miguelangeltm@live.com>
2022-02-08 11:47:33 -0500

    Updates

--- src/components/Atoms/image/CardImage/CardImage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './CardImage.css';
-
-const CardImage = (props) => {
-  return <img className="cover" src={props.source} alt="issue-cover" />;
-};
-
-export default CardImage;
